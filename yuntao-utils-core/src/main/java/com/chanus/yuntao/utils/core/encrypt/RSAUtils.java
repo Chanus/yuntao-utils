ffbf234bb2ab917194c9c67380849840b8de2cbe
ChanusYun <chanus1990@gmail.com>
2020-07-16 10:04:19 +0800

    重构 RSA 加密解密工具类，支持多种位数密钥

--- yuntao-utils-core/src/main/java/com/chanus/yuntao/utils/core/encrypt/RSAUtils.java
+++ yuntao-utils-core/src/main/java/com/chanus/yuntao/utils/core/encrypt/RSAUtils.java
@@ -20,6 +20,7 @@ import java.io.ByteArrayOutputStream;
 import java.security.*;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
@@ -37,22 +38,12 @@ public class RSAUtils {
     /**
      * 加密算法 RSA
      */
-    private static final String KEY_ALGORITHM = "RSA";
+    private static final String RSA_ALGORITHM = "RSA";
 
     /**
      * 签名算法
      */
     private static final String SIGNATURE_ALGORITHM = "MD5withRSA";
-
-    /**
-     * RSA 最大加密明文大小
-     */
-    private static final int MAX_ENCRYPT_BLOCK = 117;
-
-    /**
-     * RSA 最大解密密文大小
-     */
-    private static final int MAX_DECRYPT_BLOCK = 128;
 
     /**
      * 获取公钥的 key
@@ -67,15 +58,16 @@ public class RSAUtils {
     /**
      * 生成密钥对(公钥和私钥)
      *
+     * @param keySize 密钥位数，512/1024/2048/4096 bit
      * @return RSA 密钥对，由 Base64 编码，{@code PUBLIC_KEY} 对应公钥，{@code PRIVATE_KEY} 对应私钥
      */
-    public static Map<String, String> generateKeyPair() {
+    public static Map<String, String> generateKeyPair(int keySize) {
         try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(KEY_ALGORITHM);
-            keyPairGenerator.initialize(1024);
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(RSA_ALGORITHM);
+            keyPairGenerator.initialize(keySize);
             KeyPair keyPair = keyPairGenerator.generateKeyPair();
-            RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-            RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+            Key publicKey = keyPair.getPublic();
+            Key privateKey = keyPair.getPrivate();
             Map<String, String> keyMap = new HashMap<>(2);
             keyMap.put(PUBLIC_KEY, Base64.getEncoder().encodeToString(publicKey.getEncoded()));
             keyMap.put(PRIVATE_KEY, Base64.getEncoder().encodeToString(privateKey.getEncoded()));
@@ -85,41 +77,88 @@ public class RSAUtils {
         }
     }
 
-    private static byte[] cipher(byte[] data, Cipher cipher, int block) throws Exception {
+    /**
+     * 生成1024位密钥对(公钥和私钥)
+     *
+     * @return RSA 密钥对，由 Base64 编码，{@code PUBLIC_KEY} 对应公钥，{@code PRIVATE_KEY} 对应私钥
+     */
+    public static Map<String, String> generateKeyPair() {
+        return generateKeyPair(1024);
+    }
+
+    /**
+     * 得到公钥
+     *
+     * @param publicKey 密钥字符串（经过 Base64 编码）
+     * @return 公钥
+     */
+    public static RSAPublicKey getPublicKey(String publicKey) {
+        try {
+            // 通过 X509 编码的 Key 指令获得公钥对象
+            KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
+            X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(publicKey));
+            return (RSAPublicKey) keyFactory.generatePublic(x509KeySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException("NoSuchAlgorithmException or InvalidKeySpecException occurred.", e);
+        }
+    }
+
+    /**
+     * 得到私钥
+     *
+     * @param privateKey 密钥字符串（经过 Base64 编码）
+     * @return 私钥
+     */
+    public static RSAPrivateKey getPrivateKey(String privateKey) {
+        try {
+            // 通过 PKCS#8 编码的 Key 指令获得私钥对象
+            KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
+            PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKey));
+            return (RSAPrivateKey) keyFactory.generatePrivate(pkcs8KeySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException("NoSuchAlgorithmException or InvalidKeySpecException occurred.", e);
+        }
+    }
+
+    private static byte[] cipher(byte[] data, Cipher cipher, int maxBlock) throws Exception {
         int dataLength = data.length;
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         int offSet = 0;
         byte[] cache;
         int i = 0;
         while (dataLength - offSet > 0) {
-            if (dataLength - offSet > block) {
-                cache = cipher.doFinal(data, offSet, block);
+            if (dataLength - offSet > maxBlock) {
+                cache = cipher.doFinal(data, offSet, maxBlock);
             } else {
                 cache = cipher.doFinal(data, offSet, dataLength - offSet);
             }
             out.write(cache, 0, cache.length);
             i++;
-            offSet = i * block;
+            offSet = i * maxBlock;
         }
         byte[] encryptedData = out.toByteArray();
         out.close();
         return encryptedData;
     }
 
-    private static byte[] encrypt(byte[] data, KeyFactory keyFactory, Key key) throws Exception {
-        // 对数据加密
-        Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
-        cipher.init(Cipher.ENCRYPT_MODE, key);
-
-        return cipher(data, cipher, MAX_ENCRYPT_BLOCK);
+    private static int getMaxBlock(int mode, int keySize) {
+        return mode == Cipher.DECRYPT_MODE ? (keySize / 8) : (keySize / 8 - 11);
     }
 
-    private static byte[] decrypt(byte[] data, KeyFactory keyFactory, Key key) throws Exception {
+    private static byte[] encrypt(byte[] data, Key key, int maxBlock) throws Exception {
+        // 对数据加密
+        Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+
+        return cipher(data, cipher, maxBlock);
+    }
+
+    private static byte[] decrypt(byte[] data, Key key, int maxBlock) throws Exception {
         // 对数据解密
-        Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, key);
 
-        return cipher(data, cipher, MAX_DECRYPT_BLOCK);
+        return cipher(data, cipher, maxBlock);
     }
 
     /**
@@ -131,12 +170,8 @@ public class RSAUtils {
      */
     public static byte[] encryptByPublicKey(byte[] data, String publicKey) {
         try {
-            byte[] keyBytes = Base64.getDecoder().decode(publicKey);
-            X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(keyBytes);
-            KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
-            Key key = keyFactory.generatePublic(x509EncodedKeySpec);
-
-            return encrypt(data, keyFactory, key);
+            RSAPublicKey rsaPublicKey = getPublicKey(publicKey);
+            return encrypt(data, rsaPublicKey, rsaPublicKey.getModulus().bitLength());
         } catch (Exception e) {
             throw new RuntimeException("encrypt by public key error.", e);
         }
@@ -162,12 +197,8 @@ public class RSAUtils {
      */
     public static byte[] encryptByPrivateKey(byte[] data, String privateKey) {
         try {
-            byte[] keyBytes = Base64.getDecoder().decode(privateKey);
-            PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(keyBytes);
-            KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
-            Key key = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
-
-            return encrypt(data, keyFactory, key);
+            RSAPrivateKey rsaPrivateKey = getPrivateKey(privateKey);
+            return encrypt(data, rsaPrivateKey, rsaPrivateKey.getModulus().bitLength());
         } catch (Exception e) {
             throw new RuntimeException("encrypt by private key error.", e);
         }
@@ -193,12 +224,8 @@ public class RSAUtils {
      */
     public static byte[] decryptByPublicKey(byte[] data, String publicKey) {
         try {
-            byte[] keyBytes = Base64.getDecoder().decode(publicKey);
-            X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(keyBytes);
-            KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
-            Key key = keyFactory.generatePublic(x509EncodedKeySpec);
-
-            return decrypt(data, keyFactory, key);
+            RSAPublicKey rsaPublicKey = getPublicKey(publicKey);
+            return decrypt(data, rsaPublicKey, rsaPublicKey.getModulus().bitLength());
         } catch (Exception e) {
             throw new RuntimeException("decrypt by public key error.", e);
         }
@@ -224,12 +251,8 @@ public class RSAUtils {
      */
     public static byte[] decryptByPrivateKey(byte[] data, String privateKey) {
         try {
-            byte[] keyBytes = Base64.getDecoder().decode(privateKey);
-            PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(keyBytes);
-            KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
-            Key key = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
-
-            return decrypt(data, keyFactory, key);
+            RSAPrivateKey rsaPrivateKey = getPrivateKey(privateKey);
+            return decrypt(data, rsaPrivateKey, rsaPrivateKey.getModulus().bitLength());
         } catch (Exception e) {
             throw new RuntimeException("decrypt by private key error.", e);
         }
@@ -257,7 +280,7 @@ public class RSAUtils {
         try {
             byte[] keyBytes = Base64.getDecoder().decode(privateKey);
             PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(keyBytes);
-            KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+            KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
             PrivateKey key = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
             Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
             signature.initSign(key);
@@ -291,7 +314,7 @@ public class RSAUtils {
         try {
             byte[] keyBytes = Base64.getDecoder().decode(publicKey);
             X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(keyBytes);
-            KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+            KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
             PublicKey key = keyFactory.generatePublic(x509EncodedKeySpec);
             Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
             signature.initVerify(key);
